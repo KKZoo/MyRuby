ad1d46f55101a374501db0c7e836f0299367ad14
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2016-07-02 02:21:39 +0900

    Modify calculate of win rate
    勝率の出力結果のバグを修正しました
    for文をtimes文に変更しました
    必要性のないコメントを削除しました

--- Private/Janken.rb
+++ Private/Janken.rb
@@ -4,7 +4,7 @@
      じゃんけんプログラム！
        ・「あいこでしょ」の処理を追加する <= 終了
        ・「errorじゃ」を 負け or あいこ の処理にする？ <= (もう少し改良が必要)
-       ・勝率計算を修正する
+       ・勝率計算を修正する <= 終了
        ・UIをもっと見やすくする(考え中)
             ・出力結果をクリアして新しいじゃんけんに入る　とか
 =end
@@ -22,13 +22,13 @@ delete
 create
 
 #繰り返し処理
-   for e in 1..ARGV[0].to_i
+   (ARGV[0].to_i).times do
 
 tes=[]
 
 #入力処理
     puts "✊　＝0  , ✌️　＝1  ,✋　＝2　を入力してください"
-    tes = scanf("%d")#STDIN.gets.chomp.to_i
+    tes = scanf("%d")
     tes = tes[0]
 
 #CPU入力の分岐処理
@@ -43,7 +43,7 @@ loop do
                 tes = tes[0]
            when 1 then
                 puts "負けだよ!!"
-                `echo "-1">>./memory.txt`
+                `echo "0">>./memory.txt`
                 break
            when 2 then
                 puts "勝ちだよ!!"
@@ -68,7 +68,7 @@ loop do
                 tes = tes[0]
            when 2 then
                 puts "負けだよ!!"
-                `echo "-1">>./memory.txt`
+                `echo "0">>./memory.txt`
                 break
            else
                 puts "⚠️ ✊　＝0  , ✌️　＝1  ,✋　＝2　を入力してください"
@@ -81,7 +81,7 @@ loop do
        case tes
            when 0 then
                 puts "負けだよ!!"
-                `echo "-1">>./memory.txt`
+                `echo "0">>./memory.txt`
                 break
            when 1 then
                 puts "勝ちだよ!!"
@@ -161,16 +161,17 @@ end  #def main 終了
 
 # resultを計算
    def  result
-    data1=[] , k = 0
-    tmp_win = []  , tmp_lose = []
+    k = 0
+    tmp_win = []
+    tmp_lose = []
 
     file = open("./memory.txt")
-        data = file.readlines #1行ずつデータを取り出す処理
+        data = file.readlines
 
 #試合の勝ち負けを解析
            data.each do |line|
-                tmp_win << 0 if(data[k].to_i==1)
-                tmp_lose << 0 if(data[k].to_i==-1)
+                tmp_win << 0 if(data[k].to_i == 1)
+                tmp_lose << 0 if(data[k].to_i == 0)
                 k +=1
            end
 
