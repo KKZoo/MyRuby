d9114398ea60d1ce5034cbad33553c1ded5342cb
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2018-05-27 16:04:55 +0900

    update
    Change static program to dynamic program

--- Private/kiyoshi/test.rb
+++ Private/kiyoshi/test.rb
@@ -1,32 +1,11 @@
-p `ruby ~/kiyoshi.rb 0 0 0 0 0`
-p `ruby ~/kiyoshi.rb 0 0 0 0 1`
-p `ruby ~/kiyoshi.rb 0 0 0 1 0`
-p `ruby ~/kiyoshi.rb 0 0 0 1 1`
-p `ruby ~/kiyoshi.rb 0 0 1 0 0`
-p `ruby ~/kiyoshi.rb 0 0 1 0 1`
-p `ruby ~/kiyoshi.rb 0 0 1 1 0`
-p `ruby ~/kiyoshi.rb 0 0 1 1 1`
-p `ruby ~/kiyoshi.rb 0 1 0 0 0`
-p `ruby ~/kiyoshi.rb 0 1 0 0 1`
-p `ruby ~/kiyoshi.rb 0 1 0 1 0`
-p `ruby ~/kiyoshi.rb 0 1 0 1 1`
-p `ruby ~/kiyoshi.rb 0 1 1 0 0`
-p `ruby ~/kiyoshi.rb 0 1 1 0 1`
-p `ruby ~/kiyoshi.rb 0 1 1 1 0`
-p `ruby ~/kiyoshi.rb 0 1 1 1 1`
-p `ruby ~/kiyoshi.rb 1 0 0 0 0`
-p `ruby ~/kiyoshi.rb 1 0 0 0 1`
-p `ruby ~/kiyoshi.rb 1 0 0 1 0`
-p `ruby ~/kiyoshi.rb 1 0 0 1 1`
-p `ruby ~/kiyoshi.rb 1 0 1 0 0`
-p `ruby ~/kiyoshi.rb 1 0 1 0 1`
-p `ruby ~/kiyoshi.rb 1 0 1 1 0`
-p `ruby ~/kiyoshi.rb 1 0 1 1 1`
-p `ruby ~/kiyoshi.rb 1 1 0 0 0`
-p `ruby ~/kiyoshi.rb 1 1 0 0 1`
-p `ruby ~/kiyoshi.rb 1 1 0 1 0`
-p `ruby ~/kiyoshi.rb 1 1 0 1 1`
-p `ruby ~/kiyoshi.rb 1 1 1 0 0`
-p `ruby ~/kiyoshi.rb 1 1 1 0 1`
-p `ruby ~/kiyoshi.rb 1 1 1 1 0`
-p `ruby ~/kiyoshi.rb 1 1 1 1 1`
+=begin
+デバッグ用プログラム
+=end
+
+binary = 0
+
+32.times do
+	binary.to_s(2)
+	p `ruby kiyoshi.rb #{binary[4]} #{binary[3]} #{binary[2]} #{binary[1]} #{binary[0]}`
+	binary += 1
+end
