701a33c8cdf7e57ff557141af2a1272fffb9a0db
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2019-01-24 16:46:24 +0900

    for文をeach文に修正

--- Private/kiyoshi/kiyoshi.rb
+++ Private/kiyoshi/kiyoshi.rb
@@ -4,21 +4,21 @@ zundoko = []
 num = 0
 
 # ズンドコ節をランダムに生成
-for i in 0...5
-   #zundoko << rand(3) #乱数生成
-   zundoko << ARGV[i].to_i
-
-    if zundoko[i] == 0 then
-         p "ズン"
-    else
-         p "ドコ"
-         num += 2**i #[0,0,0,0,1]と考えた場合 => 2^4 = 16でユニーク化できるはず？
-    end
+(0...5).each do |i|
+  #zundoko << rand(3) # 乱数生成
+  zundoko << ARGV[i].to_i
+ 
+  if zundoko[i] == 0 then
+    p "ズン"
+  else
+    p "ドコ"
+    num += 2**i
+  end
 end
 
-#判定部分
+# 判定部分
 if num == 16 then
-   p "キ・ヨ・シ!!😆"
+  p "キ・ヨ・シ!!😆"
 else
-   p "キヨシできませんでした😰"
+  p "キヨシできませんでした😰"
 end
